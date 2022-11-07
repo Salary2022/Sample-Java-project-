e9462eb059e4ffc357623089310a2aae2f2869ac
SrilekhaChanamala <srilekhachanamala1998@gmail.com>
2022-11-07 14:06:57 +0530

    my commit

---
+++ ArrayAddition.java
@@ -0,0 +1,17 @@
+public class ArrayAddition {
+	public static void main(String arg[]) {
+		int a[][]={{1,2,3},{4,1,3}};
+		int b[][]={{1,2,3},{4,1,3}};
+		
+		int c[][]=new int[2][3];
+		
+		for(int i=0;i<2;i++) {
+			for(int j=0;j<3;j++){
+			c[i][j]=a[i][j]+b[i][j];
+			
+		System.out.print(c[i][j]+" ");
+	}
+	System.out.println();
+		}
+	}
+}
